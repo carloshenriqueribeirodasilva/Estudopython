be6d0b543556eb8102a676f6f719875a0b52f071
Carlos Henrique <c_henriq@hotmail.com>
2022-12-07 10:38:25 -0300

    DobroTriploRaiz 01

---
+++ DobroTriploRaiz.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um numero: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('analisando o valor {} o dobro é {}'.format(n, d))
+print('o triplo de {} é {}'.format(n, t))
+print('a raiz quadrada de {} é {}'.format(n, r))
