58eedf029af88eaf68e2764d154b6690cf70b2ce
Carlos Henrique <c_henriq@hotmail.com>
2022-12-14 09:48:13 -0300

    VERIFICANDO UMA PALAVRA EM NOME 01

---
+++ ex25.py
@@ -0,0 +1,2 @@
+nome = str(input('Digite seu nome completo ')).strip() # STRIP REMOVE OS ESPAÇOS CASO O USUARIO COLOQUE ESPAÇO ANTES DE DIGITAR
+print('Seu nome tem silva {}'.format('SILVA' in nome.upper())) # O PROGRAMA VERIFICA SE TEM SILVA NO NOME / UPPER DEIXA TODAS AS LETRAS EM MAIUSCULAS
