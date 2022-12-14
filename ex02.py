19a53b6f1dec9f698ce11b7fb6b8422bd8efdd61
Carlos Henrique <c_henriq@hotmail.com>
2022-12-14 14:25:11 -0300

    DIGITE SEU NOME COM CORES 02

--- ex02.py
+++ ex02.py
@@ -1,3 +1,4 @@
-nome = str(input("Digite seu nome: "))
-print("É um prazer te conhecer, {}!".format(nome))
+nome = str(input("Digite seu nome: ")).strip()
+print("É um prazer te conhecer, {}{}{}!".format('\033[4;34m', nome, '\033[m'))
+
 
