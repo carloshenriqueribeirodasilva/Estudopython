c85372765657ddae6118401265606137714eab7c
Carlos Henrique <c_henriq@hotmail.com>
2022-12-15 12:59:19 -0300

    TEMPO PARA O ALISTAMENTO 01

---
+++ ex39.py
@@ -0,0 +1,17 @@
+from datetime import date
+atual = date.today().year # comando para pegar a data atual
+nascimento = int(input('Ano de nascimento: '))
+idade = atual - nascimento
+print('Quem nasceu em {} tem {} anos em {}'.format(nascimento, idade, atual))
+if idade == 18:
+    print('Você tem que alistar IMEDIATAMENTE!:')
+elif idade < 18:
+    saldo = 18 - idade
+    print('Ainda faltam {} anos para o alistamento'.format(saldo))
+    ano = idade + saldo
+    print('Seu alistamnto será em {}'.format(ano))
+elif idade > 18:
+    saldo = idade - 18
+    print('Você ja deveria ter se alistado há {} anos.'.format(saldo))
+    ano = idade - saldo
+    print('Seu alistamento foi em {}.'.format(ano))
