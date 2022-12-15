2f894c8b0d79c17a381b1e5b0db0c478238ec328
Carlos Henrique <c_henriq@hotmail.com>
2022-12-15 12:17:47 -0300

    COMPARANDO NUMEROS 01

---
+++ ex38.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+if n1 > n2:
+    print('O primeiro numero é maior!')
+elif n1 < n2:
+    print('O segundo numero é maior!')
+else:
+    print('Os dois numeros sao iguais!')
