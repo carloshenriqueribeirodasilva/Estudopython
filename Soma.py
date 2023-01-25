6a7f42bc8f5fcc944858fdc5047678b075735cea
Carlos Henrique <c_henriq@hotmail.com>
2023-01-25 15:53:40 -0300

    soma 02

--- Soma.py
+++ Soma.py
@@ -1,4 +1,4 @@
 n1 = int(input('digite um numer'))
 n2 = int(input('digite outro numer'))
 s = n1 + n2
-print('a soma entre {} e {} vale {}'.format(n1, n2, s))
+print(f'a soma entre {n1} e {n2} vale {s}')
